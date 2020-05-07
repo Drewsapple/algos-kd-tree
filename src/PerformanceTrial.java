7958a78c5f32fa29b15182fd803d0b754f3f8f36
Uri Dvir <uri.j.dvir@gmail.com>
2020-05-06 21:57:40 -0400

    Brute force trial now uses non-tree dataset

--- src/PerformanceTrial.java
+++ src/PerformanceTrial.java
@@ -1,6 +1,9 @@
 
 public class PerformanceTrial {
 	
+	static final long numRuns = 100;
+	static final long numQueries = 1000;
+	
 	public static KDTree randomKDTree(int n) {
 		KDTree tree = new KDTree();
 		for (int i = 0; i < n; i++) {
@@ -24,9 +27,6 @@ public class PerformanceTrial {
 		System.out.println("KD Tree Trial -- average query time");
 		System.out.printf("%6s\t%12s\n", "N", "time (ms)");
 		
-		final long numRuns = 100;
-		final long numQueries = 1000;
-		
 		for (int n = 32; n <= 262144; n *= 2) {
 			long totalTimeNano = 0;
 			for (int run = 0; run < numRuns; run++) {
@@ -46,42 +46,34 @@ public class PerformanceTrial {
 		return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
 	}
 	
-	private static KDNode bruteForceHelper(KDNode root, double x, double y) {
-		if (root == null) {
-			return null;
+	public static double[][] randomPoints(int n){
+		double[][] points = new double[n][2];
+		for (int i = 0; i < n; i++) {
+			points[i][0] = Math.random() * kdTreeViewer.HORIZONTAL_WINDOW_SIZE;
+			points[i][1] = Math.random() * kdTreeViewer.VERTICAL_WINDOW_SIZE;
 		}
-		KDNode closest = root;
-		double distance = distSquared(root.pt.x, root.pt.y, x, y);
-		
-		KDNode big = bruteForceHelper(root.bigger, x, y);
-		if (big != null) {
-			double bigDistance = distSquared(big.pt.x, big.pt.y, x, y);
-			if (bigDistance < distance) {
-				closest = big;
-				distance = bigDistance;
-			}
-		}
-		
-		KDNode small = bruteForceHelper(root.smaller, x, y);
-		if (small != null) {
-			double smallDistance = distSquared(small.pt.x, small.pt.y, x, y);
-			if (smallDistance < distance) {
-				closest = small;
-			}
-		}
-		
-		return closest;
+		return points;
 	}
 	
-	public static KDNode bruteForceQuery(KDTree tree, double x, double y) {
-		return bruteForceHelper(tree.root, x, y);
+	public static double[] bruteForceQuery(double[][] points, double x, double y) {
+		double[] nearest = new double[2];
+		double minDist = Double.MAX_VALUE;
+		for (int i = 0; i < points.length; i++) {
+			double dist = distSquared(points[i][0], points[i][1], x, y);
+			if (dist < minDist) {
+				minDist = dist;
+				nearest[0] = points[i][0];
+				nearest[1] = points[i][1];
+			}
+		}
+		return nearest;
 	}
 	
-	public static long timeRandomBruteQuery(KDTree tree) {
+	public static long timeRandomBruteQuery(double[][] points) {
 		long start = System.nanoTime();
 		double x = Math.random() * kdTreeViewer.HORIZONTAL_WINDOW_SIZE;
 		double y = Math.random() * kdTreeViewer.VERTICAL_WINDOW_SIZE;
-		bruteForceQuery(tree, x, y);
+		bruteForceQuery(points, x, y);
 		long end = System.nanoTime();
 		return end - start;
 	}
@@ -90,15 +82,12 @@ public class PerformanceTrial {
 		System.out.println("Brute Force Trial -- average query time");
 		System.out.printf("%6s\t%12s\n", "N", "time (ms)");
 		
-		final long numRuns = 10;
-		final long numQueries = 100;
-		
 		for (int n = 32; n <= 262144; n *= 2) {
 			long totalTimeNano = 0;
 			for (int run = 0; run < numRuns; run++) {
-				KDTree tree = randomKDTree(n);
+				double[][] points = randomPoints(n);
 				for (int query = 0; query < numQueries; query++) {
-					totalTimeNano += timeRandomBruteQuery(tree);
+					totalTimeNano += timeRandomBruteQuery(points);
 				}
 			}
 			double averageTimeMillis = (double) totalTimeNano / (numRuns * numQueries * 1000000);
